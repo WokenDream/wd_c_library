f7792175637a10d731b4a96cc86b86bc85cc3280
Tianqi Liu <tianqi.liu@mail.utoronto.ca>
2018-01-07 22:32:22 -0500

    more containers added, bug fix

--- container.c
+++ container.c
@@ -4,6 +4,285 @@
 
 #include "container.h"
 #include "string.h"
+
+struct wd_stack {
+    size_t cap;
+    size_t len;
+    size_t ele_size;
+    char* array; // may be char*, will see
+};
+
+struct wd_stack* create_stack(size_t ele_size) {
+
+    struct wd_stack* stack = (struct wd_stack* )malloc(sizeof(*stack));
+    if (stack == NULL) {
+        return NULL;
+    }
+    size_t cap = 8;
+    stack->cap = cap;
+    stack->len = 0;
+    stack->ele_size = ele_size;
+    stack->array = (char* )malloc(ele_size * cap);
+    if (stack->array == NULL) {
+        return NULL;
+    }
+    return stack;
+}
+
+int ensure_stack_capacity(struct wd_stack* stack, size_t new_cap) {
+    if (stack == NULL) {
+        return -1;
+    }
+    if (stack->cap >= new_cap) {
+        return stack->cap;
+    }
+    stack->array = (char* )realloc(stack->array, stack->ele_size * new_cap);
+    if (stack->array == NULL) {
+        return -2;
+    }
+    stack->cap = new_cap;
+    return 0;
+}
+
+int stack_push(struct wd_stack* stack, void* val) {
+    if (stack == NULL || val == NULL) {
+        return -1;
+    }
+    if (stack->len == stack->cap) {
+        size_t new_cap = stack->cap << 1;
+        stack->array = (char* )realloc(stack->array, stack->ele_size * new_cap);
+        if (stack->array == NULL) {
+            return -2;
+        }
+        stack->cap = new_cap;
+    }
+    memcpy(stack->array + stack->ele_size * stack->len, val, stack->ele_size);
+    ++(stack->len);
+    return 0;
+}
+
+void* stack_pop(struct wd_stack* stack) {
+    if (stack == NULL) {
+        return NULL;
+    }
+    void* popped_val = malloc(stack->ele_size);
+    if (popped_val == NULL) {
+        return NULL;
+    }
+    memcpy(popped_val, stack->array + stack->ele_size * (stack->len - 1), stack->ele_size);
+    --(stack->len);
+    size_t half_cap = stack->cap >> 1;
+    if (stack->len < half_cap) {
+        stack->array = (char* )realloc(stack->array, stack->ele_size * half_cap);
+        stack->cap = half_cap;
+    }
+    return popped_val;
+}
+
+int stack_clear(struct wd_stack* stack) {
+    if (stack == NULL) {
+        return -1;
+    }
+    size_t cap = 4;
+    stack->len = 0;
+    stack->cap = cap;
+    stack->array = realloc(stack->array, stack->ele_size * cap);
+    return 0;
+}
+
+void stack_destroy(struct wd_stack** stack) {
+    if (stack == NULL || * stack == NULL) {
+        return;
+    }
+    free((*stack)->array);
+    free(stack);
+    *stack = NULL;
+}
+
+size_t stack_len(struct wd_stack* stack) {
+    if (stack == NULL) {
+        return 0;
+    }
+    return stack->len;
+}
+
+struct list_node {
+    void* val;
+    struct list_node* next;
+};
+
+struct linked_list_t {
+    struct list_node* head;
+    struct list_node* tail;
+    size_t ele_size;
+    size_t len;
+};
+
+struct linked_list_t* create_linked_list(size_t ele_size) {
+    struct linked_list_t* list = (struct linked_list_t* )malloc(sizeof(*list));
+    if (list == NULL) {
+        return NULL;
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->ele_size = ele_size;
+    list->len = 0;
+    return list;
+}
+
+int linked_list_prepend(struct linked_list_t* list, void* val) {
+    if (list == NULL || val == NULL) {
+        return -1;
+    }
+    struct list_node* temp = (struct list_node* )malloc(sizeof(*temp));
+    if (temp == NULL) {
+        return -2;
+    }
+    temp->val = malloc(sizeof(list->ele_size));
+    if (temp->val == NULL) {
+        return -2;
+    }
+    memcpy(temp->val, val, list->ele_size);
+    temp->next = list->head;
+
+    list->head = temp;
+    if (list->tail == NULL) {
+        list->tail = temp;
+    }
+    ++(list->len);
+    return 0;
+}
+
+int linked_list_append(struct linked_list_t* list, void* val) {
+    if (list == NULL || val == NULL) {
+        return -1;
+    }
+    struct list_node* temp = (struct list_node* )malloc(sizeof(*temp));
+    if (temp == NULL) {
+        return -2;
+    }
+    temp->val = malloc(sizeof(list->ele_size));
+    if (temp->val == NULL) {
+        return -2;
+    }
+    memcpy(temp->val, val, list->ele_size);
+    temp->next = NULL;
+
+    if (list->tail == NULL) {
+        list->head = temp;
+        list->tail = temp;
+    } else {
+        list->tail->next = temp;
+        list->tail = temp;
+    }
+    ++(list->len);
+    return 0;
+}
+
+void* linked_list_remove_first(struct linked_list_t* list) {
+    if (list == NULL || list->head == NULL) {
+        return NULL;
+    }
+    struct list_node* first = list->head;
+    void* val = malloc(sizeof(list->ele_size));
+    if (val == NULL) {
+        return NULL;
+    }
+    memcpy(val, first->val, list->ele_size);
+
+    list->head = first->next;
+    if (list->tail == first) {
+        list->tail = NULL;
+    }
+    free(first);
+    --(list->len);
+    return val;
+
+}
+
+void* linked_list_remove_last(struct linked_list_t* list) {
+    if (list == NULL || list->head == NULL) {
+        return NULL;
+    }
+    struct list_node* last = list->tail;
+    void* val = malloc(sizeof(list->ele_size));
+    if (val == NULL) {
+        return NULL;
+    }
+    memcpy(val, last->val, list->ele_size);
+
+    if (list->head == last) {
+        list->head = NULL;
+        list->tail = NULL;
+    } else {
+        struct list_node* itr = list->head;
+        while (itr->next != last) {
+            itr = itr->next;
+        }
+        list->tail = itr;
+    }
+    free(last);
+    --(list->len);
+    return val;
+}
+
+void* linked_list_remove_ith(struct linked_list_t* list, size_t index) {
+    if (index >= list->len) {
+        return NULL;
+    }
+    if (index == 0) {
+        return linked_list_remove_first(list);
+    }
+    if (index == (list->len - 1)) {
+        return linked_list_remove_last(list);
+    }
+    struct list_node* fast;
+    struct list_node* slow = list->head;
+    int i = 1;
+    while (i < index) {
+        slow = slow->next;
+//		fast = fast->next;
+        ++i;
+    }
+    fast = slow->next;
+    void* val = malloc(sizeof(list->ele_size));
+    if (val == NULL) {
+        return NULL;
+    }
+    memcpy(val, fast->val, list->ele_size);
+
+    slow->next = fast->next;
+    free(fast);
+    --(list->len);
+    return val;
+
+}
+
+size_t linked_list_len(struct linked_list_t* list) {
+    if (list == NULL) {
+        return 0;
+    }
+    return list->len;
+}
+
+int fibonacci(int i) {
+    if (i < 1) {
+        return -1;
+    } else if (i < 3) {
+        return 1;
+    }
+    int n_2 = 1, n_1 = 1;
+    int j = 2, n;
+    while (j < i) {
+        n = n_2 + n_1;
+        n_2 = n_1;
+        n_1 = n;
+        ++j;
+    }
+    return n;
+}
+
 struct wd_queue {
     size_t ele_size;
     size_t head;
@@ -81,7 +360,7 @@ struct wd_deque {
 };
 
 struct wd_deque* wd_create_deque(size_t ele_size) {
-    struct wd_deque* deque = (struct wd_array_deque* )malloc(sizeof(*deque));
+    struct wd_deque* deque = (struct wd_deque* )malloc(sizeof(*deque));
     if (deque == NULL) {
         return NULL;
     }
@@ -111,6 +390,7 @@ int wd_deque_add_last(struct wd_deque* deque, void* data) {
         }
     }
     memcpy(deque->array + (deque->tail)++ * ele_size, data, ele_size);
+    return 0;
 }
 
 int wd_deque_remove_last(struct wd_deque* deque, void* data) {
@@ -176,6 +456,7 @@ int wd_deque_remove_first(struct wd_deque* deque, void* data) {
         deque->base -= surplus;
         deque->array = new_array;
     }
+    return 0;
 }
 
 int wd_deque_size(struct wd_deque* deque) {
@@ -252,6 +533,7 @@ int wd_heap_peak(struct wd_heap* heap, void* data) {
         return -1;
     }
     memcpy(data, heap->array, heap->ele_size);
+    return 0;
 }
 
 int wd_heap_pop(struct wd_heap* heap, void* data) {
@@ -297,7 +579,7 @@ int wd_heap_insert(struct wd_heap* heap, void* data) {
     void* parent = malloc(ele_size);
     memcpy(parent, array + parent_index * ele_size, ele_size);
     memcpy(me, array + my_index * ele_size, ele_size);
-    while (parent_index >= 0 && compare(parent, me) < 0 ) {
+    while (/*parent_index >= 0 &&*/ compare(parent, me) < 0 ) {
         memcpy(array + parent_index * ele_size, me, ele_size);
         memcpy(array + my_index * ele_size, parent, ele_size);
         my_index = parent_index;
--- container.h
+++ container.h
@@ -6,12 +6,36 @@
 #define WD_LIBRARY_CONTAINER_H
 
 #include <stdlib.h>
+
+struct wd_stack;
+
+struct wd_stack* create_stack(size_t ele_size);
+int ensure_stack_capacity(struct wd_stack* stack, size_t num_of_element);
+int stack_push(struct wd_stack* stack, void* val);
+void* stack_pop(struct wd_stack* stack);
+int stack_clear(struct wd_stack* stack);
+void stack_destroy(struct wd_stack** stack);
+size_t stack_len(struct wd_stack* stack);
+
+
+struct linked_list_t;
+
+struct linked_list_t* create_linked_list(size_t ele_size);
+int linked_list_prepend(struct linked_list_t* list, void* val);
+int linked_list_append(struct linked_list_t* list, void* val);
+void* linked_list_remove_first(struct linked_list_t* list);
+void* linked_list_remove_last(struct linked_list_t* list);
+void* linked_list_remove_ith(struct linked_list_t* list, size_t index);
+size_t linked_list_len(struct linked_list_t* list);
+
+
 struct wd_queue;
 
 struct wd_queue* wd_create_queue(size_t ele_size);
 int wd_enqueue(struct wd_queue* queue, void* data);
 int wd_dequeue(struct wd_queue* queue, void* data);
 int wd_queue_size(struct wd_queue* queue);
+
 
 struct wd_deque;
 
@@ -21,6 +45,7 @@ int wd_deque_remove_last(struct wd_deque* deque, void* data);
 int wd_deque_add_first(struct wd_deque* deque, void* data);
 int wd_deque_remove_first(struct wd_deque* deque, void* data);
 int wd_deque_size(struct wd_deque* deque);
+
 
 struct wd_heap;
 
